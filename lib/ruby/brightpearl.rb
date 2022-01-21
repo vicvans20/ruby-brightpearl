df6bccb339cb6efb7e817ae8dcd21e9083fa59b4
vicvans20 <vicvans20@gmail.com>
2022-01-21 16:53:51 -0600

    Gem setup

---
+++ lib/ruby/brightpearl.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "brightpearl/version"
+
+module Ruby
+  module Brightpearl
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
