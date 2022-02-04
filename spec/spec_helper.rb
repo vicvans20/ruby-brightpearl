b67da5591abebd9680dd47c63293f772527c6db2
vicvans20 <vicvans20@gmail.com>
2022-02-03 21:00:15 -0600

    Setup Brightpearl.config before(:each) on tests instead of :suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,14 +35,12 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 
-  config.before(:suite) do
+  config.before(:each) do
     Brightpearl.config.api_domain = ENV["RUBY_BRIGHTPEARL_ENDPOINT"]
     Brightpearl.config.app_ref =      ENV["RUBY_BRIGHTPEARL_APP_REF"]
     Brightpearl.config.dev_ref =      ENV["RUBY_BRIGHTPEARL_DEV_REF"]
     Brightpearl.config.account =      ENV["RUBY_BRIGHTPEARL_ACCOUNT"]
-  end
-  
-  config.before(:each) do
+
     Brightpearl.config.token =        ENV["RUBY_BRIGHTPEARL_TOKEN"] # It expires
   end
 end
