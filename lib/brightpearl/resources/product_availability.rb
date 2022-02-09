445de49f7173b585ff6a742354380402e33e9fd8
vicvans20 <vicvans20@gmail.com>
2022-02-08 18:18:34 -0600

    Fix typo

--- lib/brightpearl/resources/product_availability.rb
+++ lib/brightpearl/resources/product_availability.rb
@@ -3,7 +3,7 @@ module Brightpearl
     extend Brightpearl::APIOperations::Get
     class << self
       def resource_path
-        "warehouse-service/product-availability/"
+        "warehouse-service/product-availability"
       end
       
       # https://api-docs.brightpearl.com/warehouse/product-availability/get.html
