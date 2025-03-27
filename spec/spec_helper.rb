298a522390d0298edd77246daef6655a9758a0d3
vicvans20 <vicvans20@gmail.com>
2025-03-27 16:53:46 -0600

    Allow oauth cassettes to work dynamically

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,26 +22,62 @@ VCR.configure do |config|
     # https://eu-use.brightpearl.com/public-api/barulu/order-service/order
     # https://ws-use.brightpearl.com/public-api/unimart/order-service/order
     if uri1.host.include?('brightpearl.com') && uri2.host.include?('brightpearl.com')
-      path1_parts = uri1.path.split('/')
-      path2_parts = uri2.path.split('/')
-      
-      # Replace the account name (index 2) with a placeholder
-      if path1_parts.length > 2 && path2_parts.length > 2
-        path1_parts[2] = 'ACCOUNT'
-        path2_parts[2] = 'ACCOUNT'
+      # Handle OAuth token URLs
+      if uri1.path.start_with?('/token/') && uri2.path.start_with?('/token/')
+        # For OAuth URLs like https://oauth.brightpearl.com/token/barulu
+        # We want to ignore the account name after /token/
+        path1_parts = uri1.path.split('/')
+        path2_parts = uri2.path.split('/')
+        
+        # Replace the account name (index 2) with a placeholder
+        if path1_parts.length > 2
+          path1_parts[2] = 'ACCOUNT'
+          path2_parts[2] = 'ACCOUNT'
+        end
+        
+        # Compare with normalized paths
+        uri1.host == uri2.host && path1_parts.join('/') == path2_parts.join('/')
+      # Handle regular API URLs
+      elsif uri1.path.include?('/public-api/') && uri2.path.include?('/public-api/')
+        path1_parts = uri1.path.split('/')
+        path2_parts = uri2.path.split('/')
+        
+        # Replace the account name (index 2) with a placeholder
+        if path1_parts.length > 2 && path2_parts.length > 2
+          path1_parts[2] = 'ACCOUNT'
+          path2_parts[2] = 'ACCOUNT'
+        end
+        
+        # Compare with normalized paths
+        uri1.host == uri2.host && path1_parts.join('/') == path2_parts.join('/')
+      else
+        # For other Brightpearl URLs, use standard URI comparison
+        uri1 == uri2
       end
-      
-      # Compare with normalized paths
-      uri1.host == uri2.host && path1_parts.join('/') == path2_parts.join('/')
     else
       # For non-Brightpearl URLs, use standard URI comparison
       uri1 == uri2
     end
   end
 
-  # Set default cassette options to use the custom matcher
+  # Create a custom matcher for OAuth token requests that ignores specific body parameters
+  config.register_request_matcher :oauth_body_without_credentials do |request1, request2|
+    if request1.uri.include?('oauth.brightpearl.com/token') && request2.uri.include?('oauth.brightpearl.com/token')
+      # For OAuth token requests, ignore client_id, client_secret, code, and redirect_uri
+      body1 = CGI.parse(request1.body)
+      body2 = CGI.parse(request2.body)
+      
+      # Only compare grant_type, which should be consistent
+      body1['grant_type'] == body2['grant_type']
+    else
+      # For non-OAuth requests, use standard body comparison
+      request1.body == request2.body
+    end
+  end
+
+  # Set default cassette options to use the custom matchers
   config.default_cassette_options = {
-    match_requests_on: [:method, :brightpearl_uri_without_account, :body]
+    match_requests_on: [:method, :brightpearl_uri_without_account, :oauth_body_without_credentials]
   }
 
   config.filter_sensitive_data('<APP-REF>') do |interaction|
